0e85ffa5daeb36784d7d89c0a6d454a91ad6ca62
Agustín Ventura Carrasco <agustinventura@gmail.com>
2012-05-13 21:05:05 +0200

    Initial commit.

---
+++ src/com/crankcode/activities/CrankPlayerActivity.java
@@ -0,0 +1,13 @@
+package com.crankcode.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CrankPlayerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
